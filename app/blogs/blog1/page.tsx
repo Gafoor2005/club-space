c31de5bdf3ecd9550f7df290c96d7dacf0c5564e
Gafoor <gafoorma686@gmail.com>
2024-11-05 19:11:33 +0530

    got a bug

--- app/blogs/blog1/page.tsx
+++ app/blogs/blog1/page.tsx
@@ -1,14 +1,11 @@
-"use client"
 import UserAvatar from '@/components/my-ui/user-avatar'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
 import { User, ArrowLeftIcon } from 'lucide-react'
 import Link from 'next/link'
-import router, { useRouter } from 'next/navigation'
 import React from 'react'
 
 export default function page() {
-  const router = useRouter();
   return (
     <div className='w-full flex'>
       <div className='flex flex-col min-w-24'>
